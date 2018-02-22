e75d1dadb4a0e8885cb4e2edf746cc31f8e5f26a
Goel Linux <es16btech11019@iith.ac.in>
2018-02-22 23:41:06 +0530

    p1 moving

--- archery/archery.pro
+++ archery/archery.pro
@@ -23,8 +23,10 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    myplayer1.cpp
 
-HEADERS  +=
+HEADERS  += \
+    myplayer1.h
 
 FORMS    += mainwindow.ui
--- archery/main.cpp
+++ archery/main.cpp
@@ -1,11 +1,31 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsScene>
+#include "myplayer1.h"
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+    QGraphicsScene * game = new QGraphicsScene();
+
+    myplayer1 * p1 = new myplayer1();
+
+    p1->setRect(0,0,100,100);
+
+     // add the player to the game
+
+    game->addItem(p1);
+
+    p1->setFlag(QGraphicsItem::ItemIsFocusable);
+    p1->setFocus();
+
+
+        // add a screen to visualize the game
+
+    QGraphicsView * screen = new QGraphicsView(game);
+
+    screen-> show();
+
+    return a. exec();
 }
